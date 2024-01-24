e87191528535bdca3217d0e3b973b0e392bc45b3
mjbaker474 <mjbaker474@gmail.com>
2024-01-24 10:30:04 -0500

    Initial Build

---
+++ MaxSet.py
@@ -0,0 +1,12 @@
+# Matthew Baker
+# Bakerma2@oregonstate.edu
+# CS325: Analysis of Algorithms
+# HW4: Question 1 - Max Set
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ PowerSet.py
@@ -0,0 +1,11 @@
+# Matthew Baker
+# Bakerma2@oregonstate.edu
+# CS325: Analysis of Algorithms
+# HW4: Question 2 - Power Set
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
