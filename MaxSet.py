7dd173015591c01ec574933dd7d7a48bec4f5b2a
mjbaker474 <mjbaker474@gmail.com>
2024-01-24 21:37:23 -0500

    Corrected function name and argument name

--- MaxSet.py
+++ MaxSet.py
@@ -8,30 +8,30 @@ def main():
     pass
 
 
-def max_set(arr):
+def max_independent_set(nums):
     """
     Determine the sequence consisting of non-consecutive elements in an array that produces the maximum sum.  Uses a
     dynamic programming approach that iterates through an input array and stores sub-problem solutions in a cache array.
     An array of all negative integers will return an empy array.
 
     Args:
-        arr (arr): An array of integers.
+        nums (arr): An array of integers.
     Returns:
         max_sum (arr): An array with the sequence of elements that produces the max sum.
     """
 
     # Initialize the cache array
-    n = len(arr)
+    n = len(nums)
     cache = [0] * n
-    cache[0] = arr[0]
+    cache[0] = nums[0]
 
     # Iterate through the input array and build the cache
     for i in range(1, n):
         # Ignore negatives
-        if arr[i] > 0:
-            cache[i] = max(cache[i - 1], cache[i - 2] + arr[i])
+        if nums[i] > 0:
+            cache[i] = max(cache[i - 1], cache[i - 2] + nums[i])
         else:
-            cache[i] = arr[i]
+            cache[i] = nums[i]
 
     # Check for edge cases of all negatives or a sum of 0
     if cache[n - 1] < 0:
@@ -48,7 +48,7 @@ def max_set(arr):
             if cache[i] == cache[i - 1]:
                 i -= 1
             else:
-                max_sum.append(arr[i])
+                max_sum.append(nums[i])
                 i -= 2
         max_sum.reverse()
         return max_sum
