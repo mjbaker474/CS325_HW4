68c5632306eb924ae84971b334375918bdf5a59a
mjbaker474 <mjbaker474@gmail.com>
2024-01-25 19:06:19 -0500

    Fixed a few edge cases

--- MaxSet.py
+++ MaxSet.py
@@ -5,7 +5,9 @@
 
 
 def main():
-    pass
+    print(max_independent_set([-1, 0, -1, -10, -34]
+
+))
 
 
 def max_independent_set(nums):
@@ -20,20 +22,27 @@ def max_independent_set(nums):
         max_sum (arr): An array with the sequence of elements that produces the max sum.
     """
 
-    # Initialize the cache array
+    # Determine length of input array and check for edge cases.  Empty input array returns empty array, single element
+    # array returns that element as long as it isn't negative, 2 element arrays return the greater non-negative element.
     n = len(nums)
-    cache = [0] * n
-    cache[0] = nums[0]
+    if n == 0:
+        return nums
+    if n == 1 and nums[0] >= 0:
+        return nums
+    if n == 2 and (nums[0] >= 0 or nums[1] >= 0):
+        return max(nums[0], nums[1])
 
-    # Iterate through the input array and build the cache
-    for i in range(1, n):
+    # For arrays longer than 2 elements, initialize cache and iterate through the input array to build the cache.
+    cache = [0] * n
+    cache[0:2] = nums[0:2]
+    for i in range(2, n):
         # Ignore negatives
         if nums[i] > 0:
             cache[i] = max(cache[i - 1], cache[i - 2] + nums[i])
         else:
             cache[i] = nums[i]
 
-    # Check for edge cases of all negatives or a sum of 0
+    # Check for edge cases of single element array, all negatives, or a sum of 0
     if cache[n - 1] < 0:
         return []
     elif cache[n - 1] == 0:
