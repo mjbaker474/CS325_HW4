87425b166cbf6e4397a5bab5f58c37e0b0a58882
mjbaker474 <mjbaker474@gmail.com>
2024-01-28 12:36:22 -0500

    Added comments.

--- PowerSet.py
+++ PowerSet.py
@@ -8,16 +8,25 @@ def main():
     pass
 
 
-def powerset(inputSet):
-    output_set = []
+def powerset(inputSet: list) -> list:
+    """
+    Generates the power set for a given input set. A power set is the set of all subsets that can be made using elements
+    from the original set. For example the power set of the set [1, 2] would be [[], [1], [2], [1,2]]
+    :param inputSet: list- A set of integers.
+    :return output_set: list-The powerset of the input set.
+    """
+
+    # Base case of empty set ends recursion.
     n = len(inputSet)
     if n == 0:
         return [[]]
 
-
+    # Recursive cases, each layer of recursion generates subsets af a particular size n.
+    output_set = []
     next_input_set = inputSet[0:n-1]
-
     subsets = powerset(next_input_set)
+
+    # Build the subsets.
     for subset in subsets:
         if subset not in output_set:
             output_set.append(subset)
