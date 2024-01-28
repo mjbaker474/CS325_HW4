fd4d11d5df9b1798052d79bf44e6170e3967ca96
mjbaker474 <mjbaker474@gmail.com>
2024-01-28 11:52:40 -0500

    PowerSet initial build, needs testing.

--- PowerSet.py
+++ PowerSet.py
@@ -3,8 +3,27 @@
 # CS325: Analysis of Algorithms
 # HW4: Question 2 - Power Set
 
+
 def main():
     pass
+
+
+def powerset(inputSet):
+    output_set = []
+    n = len(inputSet)
+    if n == 0:
+        return [[]]
+
+
+    next_input_set = inputSet[0:n-1]
+
+    subsets = powerset(next_input_set)
+    for subset in subsets:
+        if subset not in output_set:
+            output_set.append(subset)
+        if subset + [inputSet[n-1]] not in output_set:
+            output_set.append(subset + [inputSet[n-1]])
+    return output_set
 
 
 if __name__ == '__main__':
